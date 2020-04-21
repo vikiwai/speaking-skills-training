cf8edb4f79e86aa2749490c316d8d198558b4091
Victoria <avikiwai@gmail.com>
2020-04-21 18:34:12 +0300

    New initial commit

---
+++ speaking-skills-training/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  speaking-skills-training
+//
+//  Created by vikiwai on 21.04.2020.
+//  Copyright © 2020 Victoria Bunyaeva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
