06e7706a2560aa57a9b0448db25e7cd9bd9b0fb2
Victoria <avikiwai@gmail.com>
2020-04-22 14:16:42 +0300

    Add scene for account

---
+++ speaking-skills-training/Account/AccountViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AccountViewController.swift
+//  speaking-skills-training
+//
+//  Created by vikiwai on 22.04.2020.
+//  Copyright © 2020 Victoria Bunyaeva. All rights reserved.
+//
+
+import UIKit
+
+class AccountViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
