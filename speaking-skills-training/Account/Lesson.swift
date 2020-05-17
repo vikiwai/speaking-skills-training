3afe12164745849dc05554189e30b5eb0bf8370f
vikiwai <vabunyaeva@edu.hse.ru>
2020-05-17 11:57:43 +0300

    Add files

---
+++ speaking-skills-training/Account/Lesson.swift
@@ -0,0 +1,9 @@
+//
+//  Lesson.swift
+//  speaking-skills-training
+//
+//  Created by vikiwai on 17.05.2020.
+//  Copyright © 2020 Victoria Bunyaeva. All rights reserved.
+//
+
+import Foundation
---
+++ speaking-skills-training/Account/LessonTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  LessonTableViewCell.swift
+//  speaking-skills-training
+//
+//  Created by vikiwai on 16.05.2020.
+//  Copyright © 2020 Victoria Bunyaeva. All rights reserved.
+//
+
+import UIKit
+
+class LessonTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
