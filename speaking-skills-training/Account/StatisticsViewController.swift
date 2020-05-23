ff466ab63ee0259e81b42a41528d3f2349ce2497
vikiwai <vabunyaeva@edu.hse.ru>
2020-05-23 14:58:18 +0300

    Add 'Charts' module to display statistics

--- speaking-skills-training/Account/StatisticsViewController.swift
+++ speaking-skills-training/Account/StatisticsViewController.swift
@@ -7,15 +7,41 @@
 //
 
 import UIKit
+import Charts
 
 class StatisticsViewController: UIViewController {
-
+    @IBOutlet weak var progressSpeakingRateLabel: UILabel!
+    @IBOutlet weak var progressSpeakingRateView: LineChartView!
+    
+    @IBOutlet weak var pronunciationProgressLabel: UILabel!
+    @IBOutlet weak var pronunciationProgressView: UIView!
+    
+    let attempts = ["1", "2", "3", "4", "5", "6"]
+    let speakingRate = [123.4, 115, 137.5, 143.4, 149, 150.4]
+        
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        customizeChart(dataPoints: attempts, values: speakingRate.map{ Double($0) })
     }
     
+    func customizeChart(dataPoints: [String], values: [Double]) {
+       // Set ChartDataEntry
+        var dataEntries: [ChartDataEntry] = []
+            
+        for i in 0..<dataPoints.count {
+            let dataEntry = ChartDataEntry(x: values[i], y: Double(i))
+            dataEntries.append(dataEntry)
+        }
+        
+        let lineChartDataSet = LineChartDataSet(entries: dataEntries, label: nil)
+        let lineChartData = LineChartData(dataSet: lineChartDataSet)
+        
+        progressSpeakingRateView.data = lineChartData
+        
+        
+    }
 
     /*
     // MARK: - Navigation
