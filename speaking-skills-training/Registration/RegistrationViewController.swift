a48246675973859c9725ee4904b34d792ce30aab
vikiwai <vabunyaeva@edu.hse.ru>
2020-05-31 01:24:00 +0300

    Add post-request method for user registration

--- speaking-skills-training/Registration/RegistrationViewController.swift
+++ speaking-skills-training/Registration/RegistrationViewController.swift
@@ -18,6 +18,75 @@ class RegistrationViewController: UIViewController {
     @IBOutlet weak var emailTextField: UITextField!
     
     @IBAction func signUp(_ sender: Any) {
+        if passwordCheck() {
+            var request = URLRequest(url: URL(string: "")!) // MARK: TODO
+            request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+            request.httpMethod = "POST"
+            
+            let params: [String: String] = [
+                "login": loginTextField.text!,
+                "password": passwordTextField.text!,
+                "email": emailTextField.text!,
+                "name": firstNameTextField.text!,
+                "surname": lastNameTextField.text!
+            ]
+            
+            let encoder = JSONEncoder()
+            
+            do {
+                request.httpBody = try encoder.encode(params)
+                
+                let config = URLSessionConfiguration.default
+                let session = URLSession(configuration: config)
+                let task = session.dataTask(with: request) {
+                    (responseData, response, responseError) in guard responseError == nil else {
+                        print(responseError as Any)
+                        return
+                    }
+                    
+                    if let data = responseData, let utf8Representation = String(data: data, encoding: .utf8) {
+                        print("response: ", utf8Representation)
+                        
+                        let dict = utf8Representation.toJSON() as? [String: String]
+                        
+                        print(dict ?? "NULL")
+                        
+                        if dict!["status"]! == "200" {
+                            DispatchQueue.main.async {
+                                // self.save(token: dict!["token"]!, email: self.inputEmailField.text!)
+                                
+                                let storyBoard: UIStoryboard = UIStoryboard(name: "Registration", bundle: nil)
+                                let newViewController = storyBoard.instantiateViewController(withIdentifier: "App") as! LessonsTableViewController
+                                
+                                newViewController.modalPresentationStyle = .fullScreen
+                                
+                                self.present(newViewController, animated: true, completion: nil)
+                            }
+                        } else {
+                            print("Validation error")
+                            
+                            let alertController = UIAlertController(title: "Validation error",
+                                                                    message: "* is required",
+                                                                    preferredStyle: .alert)
+                            let okAction = UIAlertAction(title: "Okay", style: UIAlertAction.Style.default) {
+                                           UIAlertAction in NSLog("OK")
+                            }
+                            
+                            alertController.addAction(okAction)
+                            
+                            self.present(alertController, animated: true, completion: nil)
+                        }
+                        
+                    } else {
+                        print("No readable data received in response")
+                    }
+                }
+                
+                task.resume()
+            } catch {
+                print("Something was wrong with post request for registration")
+            }
+        }
     }
     
     private func passwordCheck() -> Bool {
@@ -25,7 +94,7 @@ class RegistrationViewController: UIViewController {
         
         if passwordTextField.text == reenteredPasswordTextField.text! {
             confirmed = true
-            
+        } else {
             DispatchQueue.main.async {
                 let alertController = UIAlertController(title: "Passwords don't match",
                                                         message: "The entered passwords are different, so registration is not completed",
@@ -35,8 +104,8 @@ class RegistrationViewController: UIViewController {
                 }
                 
                 alertController.addAction(okAction)
-                    self.present(alertController, animated: true, completion: nil)
-                }
+                self.present(alertController, animated: true, completion: nil)
+            }
         }
         
         return confirmed
@@ -59,4 +128,14 @@ class RegistrationViewController: UIViewController {
     }
     */
 
+}
+
+extension String {
+    func toJSON() -> Any? {
+        guard let data = self.data(using: .utf8, allowLossyConversion: true) else {
+            return nil
+        }
+        
+        return try? JSONSerialization.jsonObject(with: data, options: .mutableContainers)
+    }
 }
